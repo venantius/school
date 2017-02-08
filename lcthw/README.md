55cb028351b3909481e24b0a726c6152cb587bb8
Ursa Americanus Kermodei <venantius@gmail.com>
2017-02-07 20:18:25 -0800

    [lcthw] Exercises from Learn C the Hard Way

---
+++ lcthw/README.md
@@ -0,0 +1,3 @@
+# Learn C the Hard Way
+
+This is just the exercises from LCtHW
---
+++ lcthw/ex9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (i < 25) {
+		printf("%d", i);
+		i++;
+	}
+
+	// Need this to add a final newline
+	printf("\n");
+
+	return 0;
+}
